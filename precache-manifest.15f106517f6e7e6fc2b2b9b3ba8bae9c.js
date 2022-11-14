615b4805fbbab711da319c8ed9439ce317899607
Zombiekurama <viniciusfventura@gmail.com>
2022-11-14 00:17:14 -0300

    first commit

---
+++ precache-manifest.15f106517f6e7e6fc2b2b9b3ba8bae9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1936786f1d0ab6d52265",
+    "url": "/static/js/main.8e0bad96.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "44cc441f462406b13a74",
+    "url": "/static/css/2.48251d8e.chunk.css"
+  },
+  {
+    "revision": "44cc441f462406b13a74",
+    "url": "/static/js/2.67d86927.chunk.js"
+  },
+  {
+    "revision": "843480a4ba465fb9370a8b7c5626a56c",
+    "url": "/index.html"
+  }
+];
